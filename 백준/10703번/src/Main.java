b431d0c4843ebf33ed94600b2d3a8ca6436f7eaa
Sanghyeop Lee <d0003@naver.com>
2023-04-02 00:10:12 +0900

    10703번 푸는중..

---
+++ 백준/10703번/src/Main.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int R,S;
+    static char[][] pic;
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("res/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        R = Integer.parseInt(st.nextToken());
+        S = Integer.parseInt(st.nextToken());
+        pic = new char[R][S];
+        for(int i=0;i<R;i++){
+            String s = br.readLine();
+            for(int j=0;j<S;j++){
+                pic[i] = s.toCharArray();
+            }
+        }
+
+        drop(check());
+
+        for(int i=0;i<R;i++){
+            for(int j=0;j<S;j++){
+                sb.append(pic[i][j]);
+            }
+            sb.append('\n');
+        }
+        System.out.print(sb.toString());
+    }
+
+    static int check(){
+        int drop = Integer.MAX_VALUE;
+        for(int y=0;y<S;y++){
+            if(drop==0){break;}
+            for(int x=R-3;x>=0;x--){
+                if(pic[x][y]=='X'){
+                    int nx = x+1;
+                    while(true){
+                        if(pic[nx][y]=='.'){
+                            nx++;
+                        }else{
+                            break;
+                        }
+                    }
+                    int cnt = nx - x - 1;
+                    drop = Math.min(drop,cnt);
+                    break;
+                }
+            }
+        }
+        return drop;
+    }
+
+    static void drop(int num){
+        for(int y=0;y<S;y++){
+            for(int x=R-2;x>=0;x--){
+                if(pic[x][y]=='X'){
+                    pic[x+num][y]='X';
+                    pic[x][y]='.';
+                }
+            }
+        }
+    }
+}
